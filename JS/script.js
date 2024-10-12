1fc4a410cef9781f44b196eabe7042c18f24619d
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2024-10-13 02:21:44 +0600

    aoshfoa

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,20 @@
 const getBillionaires = () => {
   fetch("https://forbes400.onrender.com/api/forbes400/?limit=20")
     .then((response) => response.json())
-    .then((data) => showAllBillionaires(data));
+    .then(data => showAllBillionaires(data));
 };
 
 const showAllBillionaires = (items) => {
-    console.log(items)
-    for(let item of items){
-        const tableBody = document.getElementById("table-body");
-        const tr = document.createElement("tr");
-        tr.innerHTML = `<td>${item.personName}</td>
-                        <td>${item.countryOfCitizenship}</td>
-                        <td>${item.industries}</td> 
-                        <td>${item.rank}</td>
-                        <td>${item.finalWorth}</td>`;
-                      tableBody.appendChild(tr)
-                    }
- 
-};
-getBillionaires();
+  const tableBody = document.getElementById('table-body');
+  for(let item of items){
+    const randomIndex = Math.floor(Math.random() * items.length)
+    const randomData = item[randomIndex];
+    const tr = document.getElementById('tr');
+    tr.innerHTML = `
+                <td>${randomData.personName}</td>
+    `
+
+    tableBody.appendChild(tr);
+  }
+}
+
